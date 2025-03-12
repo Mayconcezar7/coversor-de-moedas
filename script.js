ebee77b50e91b2750833d347e4cd4933da99e1c8
Mayconcezar7 <maycon.cezar.souza7@gmail.com>
2025-03-12 13:31:09 -0300

    acrecentei mais moedas

--- script.js
+++ script.js
@@ -6,28 +6,46 @@ function convertValues(){
     const selectMoeda = document.querySelector(".currency-select").value
     const inputValues = document.querySelector(".inputMoeda").value
     const valorMoeda = document.querySelector(".valor")
-    const valorMoedaonvertida = document.querySelector(".valor-a-converte")
+    const valorMoedaConvertida = document.querySelector(".valor-a-converte")
 
     const ValorDolarDia = 5.79
     const ValorEuroDia = 6.27
+    const ValorLibraDia = 7.55
+    const ValorBitcoinDia = 486.885
 
     
 
 
 
     if(selectMoeda == "dolar"){
-        valorMoedaonvertida.innerHTML = new Intl.NumberFormat("en-US",{
+        valorMoedaConvertida.innerHTML = new Intl.NumberFormat("en-US",{
             style: "currency",
             currency: "USD"
         }).format(inputValues / ValorDolarDia)
     }
 
     if(selectMoeda == "euro"){
-        valorMoedaonvertida.innerHTML = new Intl.NumberFormat("de-DE",{
+        valorMoedaConvertida.innerHTML = new Intl.NumberFormat("de-DE",{
             style: "currency",
             currency: "EUR"
         }).format(inputValues / ValorEuroDia)
     }
+
+    if(selectMoeda == "libra"){
+        valorMoedaConvertida.innerHTML = new Intl.NumberFormat('en-GB', { 
+            style: 'currency', 
+            currency: 'GBP' 
+        }).format(inputValues/ValorLibraDia);
+    }
+
+    if(selectMoeda == "bitcoin"){
+        valorMoedaConvertida.innerHTML = new Intl.NumberFormat('en-US', { 
+            style: 'currency', 
+            currency: 'BTC',
+            minimumFractionDigits: 8, // Bitcoin usa até 8 casas decimais
+        }).format(inputValues/ValorBitcoinDia)
+    }
+
 
 
     valorMoeda.innerHTML = new Intl.NumberFormat("pt-BR",{
@@ -57,6 +75,20 @@ function mudarImageText(){
 
         moedaCoverter.innerHTML = "Euro"
         imgmoeda.src = "./assents/euro.png"
+        
+    }
+
+    if(selectMoedae == "libra"){
+
+        moedaCoverter.innerHTML = "Libra"
+        imgmoeda.src = "./assents/libra.png"
+        
+    }
+
+    if(selectMoedae == "bitcoin"){
+
+        moedaCoverter.innerHTML = "Bitcoin"
+        imgmoeda.src = "./assents/bitcoin.png"
         
     }
 
